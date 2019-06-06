a479a49c5275f19f3c0d2ddb2279b8faa630cdbc
Morgenstern, Daniel (init) <daniel.morgenstern@init.de>
2019-06-06 16:10:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+Jira Estimate
+=============
+
+This is little bookmarklet to sum up the estimatet time for a list of Jira issues.
+
+Installation
+------------
+
+1. Go to your browser and create a new bookmark in the bookmark bar
+2. As Title / Name, enter: "🗒 Remaining Estimate"
+3. As URL, enter: "javascript: [PASTE HERE THE SOURCE CODE FROM MAIN.JS]"
+
+Prerequisites / Preparation
+---------------------------
+
+1. Go to your Jira instance
+2. Navigate to the issues navigator / issue search / filter view
+3. At the top right corner press the button "Change view" -> Change the view to "List View"
+4. At top right corner of the issue list, press the "Columns" button and select the "remaining estimate" field
+
+Usage
+-----
+
+1. Go to your Jira instance
+2. Navigate to the issues navigator / issue search / filter view
+3. Press the new bookmark button "🗒 Remaining Estimate"
+
+=> Result: You get a popup window with the estimated remaining effort of the currently displayed issues.
---
+++ main.js
@@ -0,0 +1,41 @@
+var estimationCells = document.querySelectorAll('.aggregatetimeestimate');
+var allEstimates = [];
+var remainingEstimationSum = 0;
+var multipiers = {
+  week: 2400,
+  day: 480,
+  hour: 60,
+  minute: 1,
+};
+
+estimationCells.forEach(cell => {
+  var remainingEstimation = cell.innerHTML;
+  if (!remainingEstimation) {
+    return;
+  }
+
+  var parts = remainingEstimation.split(',');
+  allEstimates.push(...parts);
+});
+
+allEstimates.forEach(timeString => {
+  timeString = timeString.trim();
+  if(timeString.endsWith('s')) {
+    timeString = timeString.substring(0, timeString.length-1);
+  }
+
+  var timeStringParts = timeString.split(' ');
+  var value = parseInt(timeStringParts[0]);
+  var multipier = multipiers[timeStringParts[1]];
+  remainingEstimationSum += value * multipier;
+});
+
+var estimationInPT = remainingEstimationSum / (60*8);
+var outPutValue = estimationInPT.toFixed(2);
+
+var output = {
+  de: `Geschätzter restlicher Aufwand für angezeigte Tickets: ${outPutValue} PT.`,
+  en: `Estimatet remaining effort for the displayed issues: ${outPutValue} PT.`
+};
+
+alert(document.documentElement.lang in output ? output[document.documentElement.lang] : output.en);
