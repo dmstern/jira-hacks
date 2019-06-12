a0aa329bf362947896ae0d06b39e7559125eabaa
Morgenstern, Daniel (init) <daniel.morgenstern@init.de>
2019-06-12 18:43:07 +0200

    Add error handling

--- main.js
+++ main.js
@@ -1,41 +1,63 @@
-var estimationCells = document.querySelectorAll('.aggregatetimeestimate');
-var allEstimates = [];
-var remainingEstimationSum = 0;
-var multipiers = {
-  week: 2400,
-  day: 480,
-  hour: 60,
-  minute: 1,
-};
+calculateEstimate();
 
-estimationCells.forEach(cell => {
-  var remainingEstimation = cell.innerHTML;
-  if (!remainingEstimation) {
+function calculateEstimate() {
+  var userIsInNavigator = document.querySelector('.navigator-container') !== null;
+  if (!userIsInNavigator) {
+    alert(`Error: Please go to the Jira issue navigator.`);
     return;
   }
 
-  var parts = remainingEstimation.split(',');
-  allEstimates.push(...parts);
-});
-
-allEstimates.forEach(timeString => {
-  timeString = timeString.trim();
-  if(timeString.endsWith('s')) {
-    timeString = timeString.substring(0, timeString.length-1);
+  var isDetailView = document.querySelector('.details-layout') !== null;
+  if (isDetailView) {
+    alert(`Error: Please change view type to list.`);
+    return;
   }
 
-  var timeStringParts = timeString.split(' ');
-  var value = parseInt(timeStringParts[0]);
-  var multipier = multipiers[timeStringParts[1]];
-  remainingEstimationSum += value * multipier;
-});
+  var estimationCells = document.querySelectorAll('.aggregatetimeestimate');
 
-var estimationInPT = remainingEstimationSum / (60*8);
-var outPutValue = estimationInPT.toFixed(1);
+  if (estimationCells.length < 1) {
+    alert(`Error: Please enable the column 'Σ Remaining Estimate.'`);
+    return;
+  }
 
-var output = {
-  de: `Geschätzter restlicher Aufwand für angezeigte Tickets: ${outPutValue} PT.`,
-  en: `Estimated remaining effort for the displayed issues: ${outPutValue} PT.`
-};
+  var allEstimates = [];
+  var remainingEstimationSum = 0;
+  var multipiers = {
+    week: 2400,
+    day: 480,
+    hour: 60,
+    minute: 1,
+  };
 
-alert(document.documentElement.lang in output ? output[document.documentElement.lang] : output.en);
+  estimationCells.forEach(cell => {
+    var remainingEstimation = cell.innerHTML;
+    if (!remainingEstimation) {
+      return;
+    }
+    
+    var parts = remainingEstimation.split(',');
+    allEstimates.push(...parts);
+  });
+  
+  allEstimates.forEach(timeString => {
+    timeString = timeString.trim();
+    if(timeString.endsWith('s')) {
+      timeString = timeString.substring(0, timeString.length-1);
+    }
+    
+    var timeStringParts = timeString.split(' ');
+    var value = parseInt(timeStringParts[0]);
+    var multipier = multipiers[timeStringParts[1]];
+    remainingEstimationSum += value * multipier;
+  });
+  
+  var estimationInPT = remainingEstimationSum / (60*8);
+  var outPutValue = estimationInPT.toFixed(1);
+  
+  var output = {
+    de: `Geschätzter restlicher Aufwand für angezeigte Tickets: ${outPutValue} PT.`,
+    en: `Estimated remaining effort for the displayed issues: ${outPutValue} PT.`
+  };
+  
+  alert(document.documentElement.lang in output ? output[document.documentElement.lang] : output.en); 
+}
