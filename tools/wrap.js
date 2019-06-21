88057070870d94d808ea84b554a5e4aac61f0278
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2019-06-21 09:13:29 +0200

    build: output final url files

--- tools/wrap.js
+++ tools/wrap.js
@@ -7,8 +7,11 @@ function wrap(file) {
   fs.readFile(`build/min/${file}.js`, (err, data) => {
     if (err) throw err;
     const output = `javascript:${data}`;
-    fs.writeFile(`bookmark-urls/${file}`, output, 'utf8', err => {
+    const filePath = `./bookmark-urls/${file}`;
+    fs.writeFile(filePath, output, 'utf8', err => {
       if (err) throw err;
+      // eslint-disable-next-line no-console
+      console.log(`created new bookmark url ${filePath}`);
     });
   });  
 }
