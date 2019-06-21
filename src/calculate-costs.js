c0ee9f318e82105bbff1f38db9d69655b9b99632
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2019-06-21 08:58:07 +0200

    cleanup code: less multiple operations in one line

--- src/calculate-costs.js
+++ src/calculate-costs.js
@@ -11,20 +11,16 @@ try {
 
 function main() {
   const l10n = getLabels(document.documentElement.lang);
-
   const estimationCells = getEstimationCells();
   const remainingEstimationSum = sumRemainingEstimate(estimationCells);
-
   const estimationInHours = remainingEstimationSum / 60;
-  const costPerHour =
-    parseFloat(window.prompt(l10n.pleaseEnter(), costPerHourDefault / 100)) *
-    100;
+  const userInput = window.prompt(l10n.pleaseEnter(), costPerHourDefault / 100);
+  const costPerHour = parseFloat(userInput) * 100;
 
   if (isNaN(costPerHour)) {
     throw new Error(l10n.notANumber());
   }
 
   const costSum = ((costPerHour * estimationInHours) / 100).toFixed(2);
-
   alert(l10n.costResult(costSum));
 }
