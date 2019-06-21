e76e3f685bc3287d24af788822ab8350ba2fcef5
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2019-06-21 08:55:38 +0200

    cleanup code: less lines, less nesting depth

--- src/calculate-costs.js
+++ src/calculate-costs.js
@@ -3,29 +3,28 @@ import sumRemainingEstimate from './util/sumRemainingEstimate';
 import { costPerHourDefault } from './util/constants';
 import getEstimationCells from './util/getEstimationCells';
 
-main();
+try {
+  main();
+} catch (error) {
+  alert(error.message);
+}
 
 function main() {
   const l10n = getLabels(document.documentElement.lang);
 
-  try {
-    const estimationCells = getEstimationCells();
-    const remainingEstimationSum = sumRemainingEstimate(estimationCells);
+  const estimationCells = getEstimationCells();
+  const remainingEstimationSum = sumRemainingEstimate(estimationCells);
 
-    const estimationInHours = remainingEstimationSum / 60;
-    const costPerHour =
-      parseFloat(
-        window.prompt(l10n.pleaseEnter(), costPerHourDefault / 100)
-      ) * 100;
+  const estimationInHours = remainingEstimationSum / 60;
+  const costPerHour =
+    parseFloat(window.prompt(l10n.pleaseEnter(), costPerHourDefault / 100)) *
+    100;
 
-    if (isNaN(costPerHour)) {
-      throw new Error(l10n.notANumber());
-    }
-
-    const costSum = ((costPerHour * estimationInHours) / 100).toFixed(2);
-
-    alert(l10n.costResult(costSum));
-  } catch (error) {
-    alert(error.message);
+  if (isNaN(costPerHour)) {
+    throw new Error(l10n.notANumber());
   }
+
+  const costSum = ((costPerHour * estimationInHours) / 100).toFixed(2);
+
+  alert(l10n.costResult(costSum));
 }
--- src/estimate-effort.js
+++ src/estimate-effort.js
@@ -2,20 +2,17 @@ import { getLabels } from './util/l10n';
 import sumRemainingEstimate from './util/sumRemainingEstimate';
 import getEstimationCells from './util/getEstimationCells';
 
-main();
+try {
+  main();
+} catch (error) {
+  alert(error.message);
+}
 
 function main() {
   const l10n = getLabels(document.documentElement.lang);
-
-  try {
-    const estimationCells = getEstimationCells();
-    const remainingEstimationSum = sumRemainingEstimate(estimationCells);
-    
-    const estimationInPT = remainingEstimationSum / (60 * 8);
-    const outputValue = estimationInPT.toFixed(1);
-    
-    alert(l10n.estimateOutput(outputValue));
-  } catch(error) {
-    alert(error.message);
-  }
+  const estimationCells = getEstimationCells();
+  const remainingEstimationSum = sumRemainingEstimate(estimationCells);
+  const estimationInPT = remainingEstimationSum / (60 * 8);
+  const outputValue = estimationInPT.toFixed(1);
+  alert(l10n.estimateOutput(outputValue));
 }
