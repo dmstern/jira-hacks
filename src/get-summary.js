1ca43aa80d7e4fe79ccf39622821a710993de932
Morgenstern, Daniel (init) <daniel.morgenstern@init.de>
2019-11-12 18:53:36 +0100

    copy jira title to clipboard

---
+++ src/get-summary.js
@@ -0,0 +1,22 @@
+var summary = document.getElementById('summary-val');
+var key = document.querySelector('#key-val');
+var keyVal = key.text;
+var keyNode = document.createTextNode(`${keyVal} `);
+
+var range = document.createRange();
+range.selectNode(summary);
+range.insertNode(keyNode);
+
+window.getSelection().addRange(range);
+document.execCommand("copy");
+
+keyNode.parentNode.removeChild(keyNode);
+
+if (window.getSelection) {
+  if (window.getSelection().empty) {
+    window.getSelection().empty();
+  } else if (window.getSelection().removeAllRanges) {
+    window.getSelection().removeAllRanges();
+  }
+}
+
